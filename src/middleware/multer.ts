5cb1932c4b6606dc92b865c7a5bbddac481e1923
PedroScr1pt <celiscatzin2000@gmail.com>
2023-03-27 10:47:53 -0600

    Avance_27/03/2023

--- src/middleware/multer.ts
+++ src/middleware/multer.ts
@@ -2,11 +2,13 @@ import multer from 'multer'
 
 import path from 'path';
 
-    const storage = multer.diskStorage({
-        destination:'./documents',
-        filename:(req,file,cb)=>{
-            cb(null,"Document_"+file.fieldname + '_' + Date.now()+"_"+ path.extname(file.originalname))
-        },
-    })
-export default multer({storage})
+const storage = multer.diskStorage({
+    destination: './documents',
+    filename: (req, file, cb) => {
+        cb(null, "Document_" + file.fieldname + '_' + Date.now() + "_" + path.extname(file.originalname))
+    },
+})
+
+
+export default multer({ storage })
 
